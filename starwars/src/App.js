0abb5880c9ebfed46ba6a7ee2e7c75fff1a5222b
Annemarie Allen <annemarie@Annemaries-MacBook-Pro.local>
2020-05-04 17:56:24 -0800

    created axios call

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -2,29 +2,27 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
 
-const App = () {
+const App = () => {
   const [state, setState] = useState({});
 
   useEffect(() => {
-    axios
-      .get(
-        "https://rickandmortyapi.com/"
-        )
-      .then((response) => setState(response.data));
-  });
-  return <div className = "App"><img src = {state.url}/></div>
+    axios.get("https://rickandmortyapi.com/api/character/").then((response) => {
+      setState(response.data.results);
+      console.log(response.data.results);
+    });
+  }, []);
+  return (
+    <div className="App">
+      <h1> Characters</h1>
+      <img src={state.url} />
+    </div>
+  );
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
-  return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
-  );
 };
 
 export default App;
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,18 @@
 // Write your Character component here
+// import React from 'react';
+
+
+// const Characters = props => {
+//     return (
+//         <div>
+//             <h1> Characters </h1>
+//             <div className = 'characters' onClick
+
+//         </div>
+//         {props.character1.map(character =>
+//             return <)}
+           
+//     );
+// }
+
+// export default Character;
